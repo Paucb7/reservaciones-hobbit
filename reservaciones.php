99a38e3579f09f33b6c2db5088dabe790778e077
Paulina <112720866+Paucb7@users.noreply.github.com>
2023-04-19 13:01:01 -0600

    Primer Commit

---
+++ reservaciones.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Viaje a la tierra media | Reservaciones</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+    </head>
+    <body>
+        <div id="main">
+            <div id="header">
+                <div id="logo_text">
+                    <h1><a href="index.html">Viaje a la tierra media</a></h1>
+                    <h2>La aldea real de los hobbits en Nueva Zelanda</h2>
+                </div>
+            </div>
+        </div>
+        
+        <nav id="menu_bar">
+            <ul id="menu">
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="historia.html">Historia</a></li>
+                <li><a href="atractivos.html">Atractivos</a></li>
+                <li><a href="testimonios.html">Testimonios</a></li>
+                <li class="selected"><a href="reservaciones.html">Reservaciones</a></li>
+                <li><a href="conoce_mas.html">Conoce mas</a></li>
+            </ul>
+        </nav>
+
+    <div id="banner_reservaciones"></div>
+    <div id="contenido_sitio">
+    <div id="sidebar_container">
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Tolkien en Nueva Zelanda</h3>
+            <h4>Mas lugares del set de filmacion que te encantaran</h4>
+            <p class="parrafo">Hobbiton no es el unico lugar por visitar si vas a Nueva Zelanda. <br /><a href="#">Lee mas</a></p>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          
+          <div class="sidebar_base"></div>
+        </div>
+        <div class="sidebar">
+          <div class="sidebar_top"></div>
+          <div class="sidebar_item">
+            <h3>Busqueda</h3>
+            <form method="post" action="#" id="search_form">
+              <p>
+                <input class="search" type="text" name="search_field" value="Busqueda">
+                <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="css/search.png" alt="Buscar" title="Search" />
+              </p>
+            </form>
+          </div>
+          <div class="sidebar_base"></div>
+        </div>
+      </div>
+
+        <div id="contenidoPag">
+                <h1>Reserva tu aventura</h1>
+                <h3>Reserva ahora tu lugar en la comarca</h3>
+            <div class="form_settings">
+              <form>
+                <input class="date" type="date">
+                <input class="date" type="time">
+                <input class="contact" type="text" value="Nombre y apellido">
+                <input class="contact" type="email" value="Correo electronico">
+                <input class="submit" type="submit">
+              </form>
+            </div>
+        </div>
+    </div>
+        
+    <div id="pie_pagina"></div>
+        <div id="contenido_pie">
+            <p><a href="index.html">Inicio</a> | <a href="historia.html">Historia</a> | <a href="atractivos.html">Atractivos</a> | <a href="testimonios.html">Testimonios</a> | <a href="reservaciones.html">Reservaciones</a> | <a href="conoce_mas.html">Conoce mas</a></p>
+            <p>Copyright &copy; Rosa Paulina Corona Barba | paulinacbarba7@gmail.com | UDGVirtual | Principios de diseño web |</p>
+        </div>
+    
+    </body>
+</html>
